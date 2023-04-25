1617e3d2416e87b3cb0d1690a601c85cdbbf9e86
zafarjaleel1302 <mdzafarjaleel1302@gmail.com>
2023-04-25 20:05:26 +0530

    Day 5 assignment

--- JavaTraining/Calculator/src/Entrypoint.java
+++ JavaTraining/Calculator/src/Entrypoint.java
@@ -1,17 +1,16 @@
+import java.io.IOException;
 
 public class Entrypoint {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		int a=30;
-		int b=20;
+	public static void main(String[] args) throws IOException {
+		
+		
 		Maths m=new Maths();
-		m.add(a, b);
-		m.multiply(a, b);
-		m.subtract(a, b);
-		m.divide(a, b);
-		m.square(a);
-		m.cube(a);
+		m.calculation();
+		
+		
+		
+		
 
 	}
 
--- JavaTraining/Calculator/src/Maths.java
+++ JavaTraining/Calculator/src/Maths.java
@@ -1,37 +1,58 @@
+import java.io.IOException;
 
-public class Maths {
-	
-	public void add(int a,int b) {
-		int result=a+b;
-		System.out.println("Addition of two numbers :"+ result);
+public class Maths{
+	 
+	public void calculation() throws IOException{
+		int operator=Menu.show();
+		int result;
+		System.out.println("Enter first number");
+		int a=BasicInput.readInteger();
 		
-	}
-	public void multiply(int a, int b) {
-        int result=a*b;
-        System.out.println("multiply of two numbers :"+ result);
-	}
-	
-	public void subtract(int a,int b) {
+		System.out.println("Enter second number");
+		int b=BasicInput.readInteger();
+		
+		switch (operator) {	
 
-		int result=a-b;
-		System.out.println("subtraction of two numbers :"+ result);
-	}
-	public void divide(int a,int b) {
-		int result= a/b;
-		System.out.println("division of two numbers :" + result);		
-		
-	}
-	
-	public void square(int a) {
-		int result= a*a;
-		System.out.println("division of two numbers :" + result);		
-		
-	}
-	
-	public void cube(int a) {
-		int result= a*a*a;
-		System.out.println("division of two numbers :" + result);		
-		
-	}
+	      // performs addition between numbers
+	      case 1:
+	        result = a + b;
+	        System.out.println(a + " + " + b + " = " + result);
+	        break;
 
+	      // performs subtraction between numbers
+	      case 2:
+	        result = a - b;
+	        System.out.println(a + " - " + b + " = " + result);
+	        break;
+
+	      // performs multiplication between numbers
+	      case 3:
+	        result = a * b;
+	        System.out.println(a + " * " + b + " = " + result);
+	        break;
+
+	      // performs division between numbers
+	      case 4:
+	        result = a / b;
+	        System.out.println(a + " / " + b + " = " + result);
+	        break;
+	        
+	     // performs cube between numbers
+	      case 5:
+		        result = a*a*a;
+		        System.out.println(a + " Cube " + " = " + result);
+		        break;
+		        
+	      case 6:
+		        result = a*b;
+		        System.out.println(a + " Square " + " = " + result);
+		        break;      
+	      
+	      default:
+	        System.out.println("Invalid operator!");
+	        break;
+	    }
+		
+	}
+	
 }
