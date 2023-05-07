5644a383890fa063fd826a93d838e1127f9cd04c
zafarjaleel1302 <mdzafarjaleel1302@gmail.com>
2023-05-07 19:58:16 +0530

    updated entry class for calculator assignment

--- JavaTraining/MyFirstUiApp/src/Entry.java
+++ JavaTraining/MyFirstUiApp/src/Entry.java
@@ -3,28 +3,9 @@
 public class Entry {
 	
 	
-    /*spublic void actionPerformed(ActionEvent e) {    
-        String s1 = tf1.getText();    
-        String s2 = tf2.getText();    
-        int a = Integer.parseInt(s1);    
-        int b = Integer.parseInt(s2);    
-        int c = 0;    
-        if (e.getSource() == b1){    
-            c = a + b;    
-        }  
-        else if (e.getSource() == b2){    
-            c = a - b;    
-        }    
-        String result = String.valueOf(c);    
-        tf3.setText(result);    
-    }   */
-
 	public static void main(String[] args) {
 		MyFrame f=new MyFrame(true,"My First Windows",100,350,500,500);
-		//MyFrame f1=new MyFrame(true,"My Second Windows",100,350,500,200);
-		
-		//new Entry();
-		
+				
 		
 	}
 
